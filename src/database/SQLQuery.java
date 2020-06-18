b2bb32165604e626afadab126ddd18b6e4a07183
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2020-06-18 19:13:30 +0100

    Added javadoc to SQLQuery class.

--- src/database/SQLQuery.java
+++ src/database/SQLQuery.java
@@ -1,11 +1,25 @@
 package database;
 
+/**
+ * Stores all SQL queries.
+ *
+ * @author fraser
+ * @version 1.0
+ * @since 18/06/20
+ */
 public class SQLQuery {
+
+    private static DataBase dataBase = DataBase.getDataBaseInstance();
 
     private static SQLQuery sqlQuery = new SQLQuery();
 
+    /**
+     * @return SQLQuery instance.
+     */
     public SQLQuery getSqlQueryInstance() {
         return sqlQuery;
     }
+
+
 
 }
