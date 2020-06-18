ad42388cd8f075907d06f24c150cd4f3e89a921c
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2020-06-18 17:56:28 +0100

    Added SQLQuiry class

---
+++ src/database/SQLQuiry.java
@@ -0,0 +1,11 @@
+package database;
+
+public class SQLQuiry {
+
+    private static SQLQuiry sqlQuiry = new SQLQuiry();
+
+    public SQLQuiry getSqlQuiryInstance() {
+        return sqlQuiry;
+    }
+
+}
